12b138462208c8bf8ea463f4870a61db35b3f7c3
Philipp Droste <philipp.droste@yahoo.com>
2024-04-01 01:44:28 -0700

    research and csv posting

--- file_ingester.py
+++ file_ingester.py
@@ -1,1 +1,11 @@
 import streamlit as st
+import csv
+
+file_path = 'data.csv'
+
+with open(file_path, 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    
+    for row in csv_reader:
+        dat1_value = row['jobpost']
+        print(dat1_value)
